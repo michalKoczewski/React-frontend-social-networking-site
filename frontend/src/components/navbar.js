4410f8313fac192c62549091c1d98511978d8a5c
michalKoczewski <m.k0czewsk11@gmail.com>
2021-01-27 21:52:57 +0100

    navbar

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (                        
+            <nav class="navbard navbar-expand-lg navbar-dark bg-dark">                
+                <ul class="nav justify-content-center">                
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">nav1</a>
+                    </li>
+                </ul>
+            </nav>              
+        )
+    }
+}
+
+export default Navbar;
