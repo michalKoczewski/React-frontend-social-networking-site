e3d0aef8b9b8da7f87d2956d6336eeb6bcf7983d
kubasse <kubaposadzy98@gmail.com>
2021-01-28 14:12:17 +0100

    store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,32 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import { Router, Route, browserHistory } from 'react-router';
->>>>>>> parent of 9143539... Add Redux
-=======
 import { Provider } from 'react-redux';
->>>>>>> parent of 4f9297f... Merge branch 'main' of https://github.com/michalKoczewski/React-frontend-social-networking-site into main
-=======
-import { Provider } from 'react-redux';
->>>>>>> parent of 4f9297f... Merge branch 'main' of https://github.com/michalKoczewski/React-frontend-social-networking-site into main
-=======
-import { Provider } from 'react-redux';
->>>>>>> parent of 4f9297f... Merge branch 'main' of https://github.com/michalKoczewski/React-frontend-social-networking-site into main
-=======
-import { Router, Route, browserHistory } from 'react-router';
->>>>>>> parent of 9143539... Add Redux
 import './index.css';
 import Navbar from "./components/navbar";
+import App from './App';
+import store from './store';
+
 
 ReactDOM.render(  
-  <React.StrictMode>
-    <Navbar/>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.querySelector('#root')
 );
-
