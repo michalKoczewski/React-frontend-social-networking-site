2e1855280560ed4efe4adb02c7584a317d3bf7df
kubasse <kubaposadzy98@gmail.com>
2021-01-28 14:06:23 +0100

    test

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,25 +2,6 @@ import React from 'react';
 
 class Navbar extends React.Component {
     render() {        
-<<<<<<< HEAD
-        return (            
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark p-1 fixed-top">
-                <div class="container">
-                    <div class="collapse navbar-collapse fs-5" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto">
-                            <li class="nav-item px-1">
-                                <a class="nav-link active" href="#">Home Page</a>
-                            </li>
-                        </ul>
-                        <ul class="navbar-nav">
-                            <li class="nav-item px-1">
-                                <a class="nav-link" href="#">Logout</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>                 
-=======
         return (      
             <nav class="navbar navbar-expand-md navbar-dark bg-dark p-1 fixed-top">
                     <div class="container">
@@ -38,7 +19,6 @@ class Navbar extends React.Component {
                         </div>
                     </div>
                 </nav>
->>>>>>> parent of 9143539... Add Redux
         )
     }
 }
