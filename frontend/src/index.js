da52468752b644465e5f1dd4799d5ec6e8997a84
kubasse <kubaposadzy98@gmail.com>
2021-01-28 14:00:09 +0100

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import { Router, Route, browserHistory } from 'react-router';
 >>>>>>> parent of 9143539... Add Redux
+=======
+import { Provider } from 'react-redux';
+>>>>>>> parent of 4f9297f... Merge branch 'main' of https://github.com/michalKoczewski/React-frontend-social-networking-site into main
 import './index.css';
-import Navbar from "./components/navbar";
+import App from './App';
+import store from './store';
+
 
 ReactDOM.render(  
-  <React.StrictMode>
-    <Navbar/>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.querySelector('#root')
 );
 
