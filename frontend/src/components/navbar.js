1cbf01321e7465ba1147b2e1616a8687e490d2e8
michalKoczewski <m.k0czewsk11@gmail.com>
2021-01-28 13:44:01 +0100

    p

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 class Navbar extends React.Component {
     render() {        
-        return (      
+        return (            
             <nav class="navbar navbar-expand-md navbar-dark bg-dark p-1 fixed-top">
-                    <div class="container">
-                        <div class="collapse navbar-collapse fs-5" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto">
-                                <li class="nav-item px-1">
-                                    <a class="nav-link active" href="#">Home Page</a>
-                                </li>
-                            </ul>
-                            <ul class="navbar-nav">
-                                <li class="nav-item px-1">
-                                    <a class="nav-link" href="#">Logout</a>
-                                </li>
-                            </ul>
-                        </div>
+                <div class="container">
+                    <div class="collapse navbar-collapse fs-5" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item px-1">
+                                <a class="nav-link active" href="#">Home Page</a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav">
+                            <li class="nav-item px-1">
+                                <a class="nav-link" href="#">Logout</a>
+                            </li>
+                        </ul>
                     </div>
-                </nav>
+                </div>
+            </nav>                 
         )
     }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
 import './index.css';
 import Navbar from "./components/navbar";
 
