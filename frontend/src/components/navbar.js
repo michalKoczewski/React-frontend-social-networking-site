3fae3c48e0b68a8c8e7340a17f11d8aac3f8fbfd
kubasse <kubaposadzy98@gmail.com>
2021-01-28 12:29:18 +0100

    New Menu

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,18 +3,22 @@ import React from 'react';
 class Navbar extends React.Component {
     render() {        
         return (      
-            <div style={{"height" : "45px"}}>
-                <nav className="navbard h-100 navbar-expand-lg navbar-dark bg-dark">             
-                    <li className="nav justify-content-center">                
-                        <ul style={{"paddingLeft": "0px"}} className="nav-item">
-                            <a style={{"margin" : "6px"}} type="button" className="btn btn-secondary btn-sm">nav133333</a>
-                        </ul>
-                        <ul style={{"paddingLeft": "0px"}} className="nav-item">
-                            <a style={{"margin" : "6px"}} type="button" className="btn btn-secondary btn-sm">nav133333</a>
-                        </ul>
-                    </li>
-                </nav>              
-            </div>
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark p-1 fixed-top">
+                    <div class="container">
+                        <div class="collapse navbar-collapse fs-5" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto">
+                                <li class="nav-item px-1">
+                                    <a class="nav-link active" href="#">Home Page</a>
+                                </li>
+                            </ul>
+                            <ul class="navbar-nav">
+                                <li class="nav-item px-1">
+                                    <a class="nav-link" href="#">Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
         )
     }
 }
