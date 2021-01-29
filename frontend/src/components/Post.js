e39d828a63aecc9a27f2faad5011304bc382fa73
kubasse <kubaposadzy98@gmail.com>
2021-01-29 12:01:45 +0100

    Post Loading

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -5,9 +5,9 @@ export default function Post(props) {
     return (     
         <div key={post._id} className="card">
             <a href={`/posts/${post._id}`}>
-                <img src="http://localhost:8000/uploads//2021-01-06T043810.879Zpobrane.jpg" className="card-img-top" alt="brak" />
+                <img src={`http://localhost:8000/${post.img}`} className="card-img-top" alt="brak" />
                 <div className="card-body">
-                    <p className="card-text">{post._id}</p>
+                    <p className="card-text btn">{post.postDescription}</p>
                 </div>
             </a>
         </div>
--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,32 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Post from './Post';
-import LoadingBox from './LoadingBox';
-import { useDispatch, useSelector } from 'react-redux';
-import { listPosts } from '../actions/postsActions';
+import axios from 'axios';
 
 
 export default function Posts() { 
-    const dispatch = useDispatch();
-    const postList = useSelector( state => state.postList);
-    const {loading, error, posts} = postList;
-    useEffect(() => {
-        dispatch(listPosts());
-    }, [dispatch]);
+    const [posts, setPosts] = useState({info: []});
+    useEffect(async () => {
+        const result = await axios('http://localhost:8000/posts');
+        setPosts(result.data);
+
+    })
 
     return (      
         <div className="container-fluid">
             <div className="row mt-4">
                 <div className="col-3"></div>
                 <div className="col-6">
-                    {loading? (
-                        <LoadingBox />
-                    ) : error? (
-                        <div className="bg-danger">{error}</div>
-                    ) : (
-                        <div>
-                            {[posts].map(post => (<Post key={post._id} post={post} />))}
-                        </div>
-                    )}
+                    {posts.info.map(post => (<Post key={post._id} post={post} />))}
                 </div>
                 <div className="col-3"></div>
             </div>   
